8eb4600f5f24fdfd18bf2e8ecdd12b82ce0da628
Shakirzhan <samarbaev99@mail.ru>
2019-10-18 11:05:27 +0300

    v 4

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,11 +1,59 @@
-import React from "react";
+import React from 'react';
+import { Button, Form } from 'semantic-ui-react';
 
 class LoginForm extends React.Component {
-  state = {}
+  state = {
+    data: {
+      email: '',
+      password: ''
+    },
+    loading: false,
+    errors: {}
+  };
+
+  onChange = e => this.setState({ data: { ...this.state.data, [e.target.name]: e.target.value } });
+
+  onSubmit = () => {
+    const errors = this.validate(this.state.data);
+    this.setState({ errors });
+  };
+
+  validate = data => {
+    const errors = {};
+    if (!data.password) errors.password = "Can't be blank";
+    return errors;
+  };
 
   render() {
+    const { data } = this.state;
     return (
-      <div></div>
+      <Form onSubmit={this.onSubmit}>
+        <Form.Field>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="example@example.com"
+            value={data.email}
+            onChange={this.onChange}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={data.password}
+            onChange={this.onChange}
+            placeholder="pass"
+          />
+        </Form.Field>
+        <Button primary>Login</Button>
+      </Form>
     );
   }
 }
+
+export default LoginForm;
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react';
+import LoginForm from '../forms/LoginForm';
 
 const LoginPage = () => (
   <div className="ui container">
