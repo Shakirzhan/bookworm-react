dd09b5a5d95d356d7cff5902136981cb281614fc
Shakirzhan <samarbaev99@mail.ru>
2019-10-18 15:52:44 +0300

    v 5

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Button, Form } from 'semantic-ui-react';
+import Validator from 'validator';
+import InlineError from '../messages/InlineError';
 
 class LoginForm extends React.Component {
   state = {
@@ -16,19 +19,23 @@ class LoginForm extends React.Component {
   onSubmit = () => {
     const errors = this.validate(this.state.data);
     this.setState({ errors });
+    if (Object.keys(errors).length === 0) {
+      this.props.submit(this.state.data);
+    }
   };
 
   validate = data => {
     const errors = {};
+    if (!Validator.isEmail(data.email)) errors.email = 'Invalid email';
     if (!data.password) errors.password = "Can't be blank";
     return errors;
   };
 
   render() {
-    const { data } = this.state;
+    const { data, errors } = this.state;
     return (
       <Form onSubmit={this.onSubmit}>
-        <Form.Field>
+        <Form.Field error={!!errors.email}>
           <label htmlFor="email">Email</label>
           <input
             type="email"
@@ -38,8 +45,9 @@ class LoginForm extends React.Component {
             value={data.email}
             onChange={this.onChange}
           />
+          {errors.email && <InlineError text={errors.email} />}
         </Form.Field>
-        <Form.Field>
+        <Form.Field error={!!errors.password}>
           <label htmlFor="password">Password</label>
           <input
             type="password"
@@ -49,11 +57,16 @@ class LoginForm extends React.Component {
             onChange={this.onChange}
             placeholder="pass"
           />
+          {errors.password && <InlineError text={errors.password} />}
         </Form.Field>
         <Button primary>Login</Button>
       </Form>
     );
   }
 }
+
+LoginForm.propTypes = {
+  submit: PropTypes.func.isRequired
+};
 
 export default LoginForm;
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import LoginForm from '../forms/LoginForm';
 
-const LoginPage = () => (
-  <div className="ui container">
-    <h1>LoginPage</h1>
-    <LoginForm />
-  </div>
-);
+class LoginPage extends React.Component {
+  submit = data => {
+    console.log(data);
+  };
+  render() {
+    return (
+      <div className="ui container">
+        <h1>LoginPage</h1>
+        <LoginForm submit={this.submit} />
+      </div>
+    );
+  }
+}
 
 export default LoginPage;
