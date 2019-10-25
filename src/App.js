00f4999506c157d09e73512cab96316a334b541b
Shakirzhan <samarbaev99@mail.ru>
2019-10-25 09:39:59 +0300

    v 9

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import { Route } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
 import DashboardPage from "./components/pages/DashboardPage";
+import SignupPage from "./components/pages/SignupPage";
+import UserRoute from "./components/routes/UserRoute";
+import GuestRoute from "./components/routes/GuestRoute";
+import PropTypes from 'prop-types';
 
-const App = () => (
+const App = ({ location }) => (
   <div className="ui container">
-    <Route path="/" exact component={HomePage} />
-    <Route path="/login" exact component={LoginPage} />
-    <Route path="/dashboard" exact component={DashboardPage} />
+    <Route location={location} path="/" exact component={HomePage} />
+    <GuestRoute location={location} path="/signup" exact component={SignupPage} />
+    <GuestRoute location={location} path="/login" exact component={LoginPage} />
+    <UserRoute location={location} path="/dashboard" exact component={DashboardPage} />
   </div>
 );
+
+App.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired
+  }).isRequired
+}
 
 export default App;
---
+++ src/components/forms/SignupForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class SignupForm extends React.Component {
+  state = {
+    data: {
+      email: '',
+      password: ''
+    },
+    loading: false,
+    errors: {}
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
